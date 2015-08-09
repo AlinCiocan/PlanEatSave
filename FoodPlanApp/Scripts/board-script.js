48263abe87108127b13801edd19e3f3c54e75de2
alin_c9 <alin_c9@yahoo.com>
2015-08-09 19:27:45 +0300

    Integrated the UI from "FoodPlan UI Prototype" into this project

---
+++ FoodPlanApp/Scripts/board-script.js
@@ -0,0 +1,33 @@
+﻿/// <reference path="jquery-2.1.4.intellisense.js" />
+
+(function () {
+    function init() {
+        console.log("hello world!!");
+
+        var dayCardWidth = 200;
+        var $daysBoard = $(".days-board"),
+            $dayCards = $daysBoard.children(".day-card");
+
+
+
+
+        // duplicate days (for prototyping purposes)
+        // $daysBoard.append($dayCards.clone());
+        // $daysBoard.append($dayCards.clone());
+
+        // I kinda need something to do me the binding
+        // because I have to query again dom to have the correct childs
+        $dayCards = $daysBoard.children(".day-card");
+
+        // calculate board width
+        var daysBoardWidth = ((dayCardWidth + 10) * $dayCards.length) + "px";
+        $daysBoard.css("width", daysBoardWidth);
+
+
+
+    }
+
+    $(document).ready(function () {
+        init();
+    });
+})();
