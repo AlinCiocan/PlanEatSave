a204fd6900a322060c7beb012a968e33184549a6
alin_c9 <alin_c9@yahoo.com>
2015-08-18 20:48:20 +0300

    Refactored the board-script-for-styling.js using angular and added watch method over board object

--- FoodPlanApp/Scripts/foodplan-board-app.js
+++ FoodPlanApp/Scripts/foodplan-board-app.js
@@ -1,4 +1,5 @@
 ﻿/// <reference path="angular.js" />
+/// <reference path="board-script-for-styling.js" />
 
 (function (angular) {
     "use strict";
@@ -10,11 +11,28 @@
         .then(function(response) {
             console.log("success", response);
             $scope.board = response.data;
+
+            $scope.styleForBoard = {
+                width : $scope.board.days.length * 220 + "px"
+            };
+
+            $scope.$watch("board", function(newBoard, oldBoard) {
+                console.log("old board", oldBoard);
+                console.log("new board", newBoard);
+
+                // TODO: do the request for put (to update the board)
+                // $http.put("url to put..", newBoard);
+
+            }, true);
+
+            $scope.changeBoard = function() {
+                $scope.board.days[0].categories[0].title = "the new category title";
+            }
+
         }, function(error) {
             console.log("error", error);
         });
 
-        $scope.boardDay = "Happy Friday";
     }]);
 
 })(window.angular);
