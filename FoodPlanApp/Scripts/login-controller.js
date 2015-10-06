cbd7ea757a3a22934714965cab54d2b6da9dd103
Alin Ciocan <alin_c9@yahoo.com>
2015-10-06 21:34:21 +0300

    Created the login flow

--- FoodPlanApp/Scripts/login-controller.js
+++ FoodPlanApp/Scripts/login-controller.js
@@ -5,13 +5,30 @@
 
     var foodplanBoardApp = angular.module("foodplanBoardApp", []);
 
-    // TODO: make sure to refactor this controller, it starts to become too big
     foodplanBoardApp.controller("LoginController",
         ["$scope", "$http", function($scope, $http) {
 
-            $scope.login = function() {
 
+            function loginRequestCallback(response) {
+                console.log(response.data);
+
+                
+            }
+
+            function loginRequestFailedCallback(error) {
+                console.log("An error ocurred while trying to do login", error);
+            }
+
+
+            $scope.login = function () {
+                var user = {
+                    username: $scope.username,
+                    password: $scope.password
+                };
+                $http.post("/api/user/login", user)
+                    .then(loginRequestCallback, loginRequestFailedCallback);
             };
+
 
         }]);
 
